5db7a930a561d3fcc80769b0d8ce20293e4c4ae4
Devin Canterberry <devin@twuni.org>
2018-03-19 16:24:34 -0700

    🐛 Fix validator in JSON-RPC middleware

--- src/middleware/jsonrpc/index.js
+++ src/middleware/jsonrpc/index.js
@@ -3,17 +3,19 @@ const HTTP_OK = 200;
 
 const validators = [
   { message: 'Missing request body', validate: (request) => Boolean(request.body) },
-  { message: 'Unsupported "jsonrpc" version', validate: (request) => request.body === '2.0' },
-  { message: 'Missing required parameter: "id"', validate: (request) => Boolean(request.body.id) },
-  { message: 'Missing required parameter: "method"', validate: (request) => Boolean(request.body.method) },
-  { message: 'Missing required parameter: "params"', validate: (request) => Boolean(request.body.params) }
+  { message: 'Unsupported "jsonrpc" version', validate: (request) => request.body && request.body.jsonrpc === '2.0' },
+  { message: 'Missing required parameter: "id"', validate: (request) => request.body && Boolean(request.body.id) },
+  { message: 'Missing required parameter: "method"', validate: (request) => request.body && Boolean(request.body.method) },
+  { message: 'Missing required parameter: "params"', validate: (request) => request.body && Boolean(request.body.params) }
 ];
 
 const jsonrpc = () => (request, response, next) => {
-  const invalidation = validators.find((validator) => !validator.validate(request));
+  const invalidations = validators.filter(({ validate }) => !validate(request));
 
-  if (invalidation) {
-    return response.status(HTTP_CLIENT_ERROR).send(invalidation.message);
+  if (invalidations.length > 0) {
+    return response.status(HTTP_CLIENT_ERROR).json({
+      errors: invalidations.map(({ message }) => ({ message }))
+    });
   }
 
   request.jsonrpc = { [request.body.method]: request.body.params };
